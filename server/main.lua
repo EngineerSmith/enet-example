824bb505899e1f5828202596c8003114c6fc7233
EngineerSmith <pasmith.bus@gmail.com>
2022-01-01 20:14:55 +0000

    Improved comments and code to server

--- server/main.lua
+++ server/main.lua
@@ -2,20 +2,26 @@ local enet = require("enet")
 local address, port = "*", 12345
 local host = enet.host_create(address..":"..tostring(port))
 
+local success = host:service(5000) -- Start server connection
+if not success then -- may fail to start if port is already in use
+  host = nil
+  love.draw = function() love.graphics.print("Could not start server on port "..port) end
+end
+
 love.update = function()
   if host then
     local event = host:service(3) -- 3ms timeout, just put it higher in a thread
-    local count, limit = 0, 50 -- Since it isn't threaded, make sure it exits update
-    while event and count < limit do
+    local count, limit = 0, 50 -- Since it isn't threaded, make sure it exits update after a reasonable number of cycles 
+    while event and count < limit do -- (love.event funcs in love.run still needs to run for server)
       if event.type == "receive" then
         host:broadcast(tostring(event.peer)..": "..event.data) -- do tostring(peer) to get ip and port it is coming from
         -- event.peer:send(data) to send data to just that client
       elseif event.type == "connect" then
-        host:broadcast(tostring(event.peer).." has joined the server!")
+        host:broadcast(tostring(event.peer).." has joined the server!") -- Tell everyone someone has joined
       elseif event.type == "disconnect" then
-        host:broadcast(tostring(event.peer).." has left the server!")
+        host:broadcast(tostring(event.peer).." has left the server!") -- Tell everyone someone has left
       end
-      event = host:service()
+      event = host:service() -- get the next event in queue - no waiting
       count = count + 1
     end
   end
