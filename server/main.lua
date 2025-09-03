edbf5a955799e4bdc4c88f21e4dfcc2af54815ce
Engineer Smith <56016593+EngineerSmith@users.noreply.github.com>
2025-09-03 23:13:36 +0100

    Fixed bug which can skip events

--- server/main.lua
+++ server/main.lua
@@ -12,7 +12,7 @@ love.update = function()
   if host then
     local event = host:service(3) -- 3ms timeout, recommended to put it higher in a thread
     local count, limit = 0, 50 -- Since it isn't threaded, make sure it exits update after a reasonable number of cycles 
-    while event and count < limit do -- (love.event funcs in love.run still needs to run for server)
+    while event do -- (love.event funcs in love.run still needs to run for server)
       if event.type == "receive" then
         host:broadcast(tostring(event.peer)..": "..event.data) -- do tostring(peer) to get ip and port it is coming from
         -- event.peer:send(data) to send data to just that client
@@ -21,8 +21,12 @@ love.update = function()
       elseif event.type == "disconnect" then
         host:broadcast(tostring(event.peer).." has left the server!") -- Tell everyone someone has left
       end
-      event = host:check_events() -- get the next event in queue - no waiting
       count = count + 1
+      if count < limit then
+        break
+      end
+      event = host:check_events() -- get the next event in queue - no waiting
     end
   end
+
 end
